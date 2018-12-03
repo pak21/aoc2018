c625626928679e7f231e8fcdbc8f414c6c93fb5d
Phil Kendall <phil.kendall@controlf1.co.uk>
2018-12-03 12:54:36 +0000

    Bit of a tidy up.

--- 03/03-1.py
+++ 03/03-1.py
@@ -1,26 +1,19 @@
 #!/usr/bin/python3
 
 import collections
+import re
 import sys
 
 claims = collections.defaultdict(lambda: collections.defaultdict(lambda: 0))
 
+pattern = re.compile(r'#\d+ @ (\d+),(\d+): (\d+)x(\d+)')
+
 with open(sys.argv[1]) as f:
     for line in f:
-        _, _, anchor, size = line.split()
-        left, top = anchor.split(',')
-        left = int(left)
-        top = int(top[:-1])
-        width, height = map(int, size.split('x'))
+        matches = pattern.match(line).groups
+        left, top, width, height = map(int, matches(0))
         for x in range(left, left + width):
             for y in range(top, top + height):
                 claims[x][y] += 1
 
-count = 0
-
-for x in claims.values():
-    for y in x.values():
-        if (y > 1):
-            count += 1
-
-print(count)
+print(sum(map(lambda row: len(list(filter(lambda i: i > 1, row.values()))), claims.values())))
--- 03/03-2.py
+++ 03/03-2.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
 
 import collections
+import re
 import sys
 
 claims = collections.defaultdict(lambda: collections.defaultdict(lambda: []))
 count = 0
 
+pattern = re.compile(r'#(\d+) @ (\d+),(\d+): (\d+)x(\d+)')
+
 with open(sys.argv[1]) as f:
     for line in f:
-        claim, _, anchor, size = line.split()
-        claim = int(claim[1:])
-        left, top = anchor.split(',')
-        left = int(left)
-        top = int(top[:-1])
-        width, height = map(int, size.split('x'))
+        matches = pattern.match(line).groups
+        claim, left, top, width, height = map(int, matches(0))
         for x in range(left, left + width):
             for y in range(top, top + height):
                 claims[x][y].append(claim)
         count += 1
 
-good = set(range(1, count + 1))
+bad = set()
+for row in claims.values():
+    for y in filter(lambda a: len(a) > 1, row.values()):
+        bad |= set(y)
 
-for x in claims.values():
-    for y in x.values():
-        if (len(y) > 1):
-            for z in y:
-                if z in good:
-                    good.remove(z)
-
-print(good)
+print(set(range(1, count + 1)) - bad)
