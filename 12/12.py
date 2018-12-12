1467813766030e198162532e1d2016c3050de43d
Philip Kendall <philip@shadowmagic.org.uk>
2018-12-12 22:15:31 +0000

    Combine both parts into one.

--- 12/12.py
+++ 12/12.py
@@ -13,10 +13,24 @@ with open(sys.argv[1]) as f:
     rules = dict([rulepattern.match(line).groups() for line in f])
 
 zerooffset = 0
-for i in range(20):
+seenstates = set()
+
+generations = 0
+while True:
+    generations += 1
     zerooffset += state.index('#') - 2
     state = '....' + remove.sub(r'\1', state) + '....'
-    newstate = ''.join([rules[state[j:j+5]] for j in range(len(state)-4)])
-    state = newstate
 
-print(sum([i + zerooffset for i, c in enumerate(state) if c == '#']))
+    state = ''.join([rules[state[j:j+5]] for j in range(len(state)-4)])
+    score = sum([i + zerooffset for i, c in enumerate(state) if c == '#'])
+
+    if generations == 20:
+        print(score)
+
+    if state in seenstates:
+        break
+
+    seenstates.add(state)
+    lastscore = score
+
+print(score + (score - lastscore) * (50000000000 - generations))
