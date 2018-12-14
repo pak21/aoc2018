b24837472f2343dbbd4e315d079e9b7433045266
Philip Kendall <philip@shadowmagic.org.uk>
2018-12-14 22:52:29 +0000

    Combine both parts.

--- 13/13.py
+++ 13/13.py
@@ -12,10 +12,11 @@ with open(sys.argv[1]) as f:
 carts = [[y, x, directions[grid[y][x]], 0] for y in range(len(grid)) for x in range(len(grid[0])) if grid[y][x] in directions]
 
 while True:
-    order = sorted(carts, key=itemgetter(1, 0))
+    carts.sort(key=itemgetter(1, 0))
 
-    for i in range(len(order)):
-        cart = order[i]
+    todelete = []
+    for i in range(len(carts)):
+        cart = carts[i]
         cart[0] += moves[cart[2]][0]
         cart[1] += moves[cart[2]][1]
 
@@ -28,10 +29,16 @@ while True:
             cart[2] = (cart[2] + 1 - cart[3]) % 4
             cart[3] = (cart[3] + 1) % 3
 
-        for j in range(len(order)):
+        for j in range(len(carts)):
             if i == j:
                 continue
-            other = order[j]
+            other = carts[j]
             if cart[0] == other[0] and cart[1] == other[1]:
-                print('{},{}'.format(cart[1], cart[0]))
-                sys.exit(0)
+                print('Crash at {},{}'.format(cart[1], cart[0]))
+                todelete += [i, j]
+
+    for d in sorted(todelete)[::-1]:
+        del carts[d]
+        if len(carts) == 1:
+            print('Final cart is at {},{}'.format(carts[0][1], carts[0][0]))
+            sys.exit(0)
