06e20e86f6efdba6ccdd7cf11c9178990b3a6de9
Philip Kendall <philip@shadowmagic.org.uk>
2018-12-22 14:08:37 +0000

    Tidy up (with one big speedup).

--- 22/22.py
+++ 22/22.py
@@ -20,77 +20,50 @@ def calculate_cell(key):
 def get_cell(key):
     if key not in grid:
         grid[key] = calculate_cell(key)
-    
     return grid[key]
 
 def get_region_type(key):
-    return get_cell((key[0], key[1])) % 3
+    if key not in regions:
+        regions[key] = get_cell((key[0], key[1])) % 3
+    return regions[key]
 
 with open(sys.argv[1]) as f:
     depth = int(f.readline().split()[1])
     tx, ty = [int(i) for i in f.readline().split()[1].split(',')]
 
 grid = {}
+regions = {}
 
 print('Part 1: {}'.format(sum([get_region_type((x, y)) for y in range(ty + 1) for x in range(tx + 1)])))
 
-start = (0, 0, True, False)
+def try_news(new):
+    for e in range(3):
+        new2 = (*new, e)
+        if get_region_type(current) == new2[2] or get_region_type(new2) == new2[2]:
+            continue
+        new_distance = distances[current] + (8 if current[2] != new2[2] else 1)
+        if new_distance < distances[new2]:
+            distances[new2] = new_distance
+            active[new2] = new_distance
 
-distances = {start: 0}
+start = (0, 0, 1)
+target = (tx, ty, 1)
+
+distances = collections.defaultdict(lambda: 8 * (tx + ty))
+distances[start] = 0
 active = {start: 0}
 current = start
 
-visited = set()
-
-def is_valid(region_type, state):
-    if region_type == 0:
-        # Rocky, must have one or the other equipped
-        return state[2] or state[3]
-    if region_type == 1:
-        # Wet, must not have the torch equipped
-        return not state[2]
-    if region_type == 2:
-        # Narrow, must not have climbing gear equipped
-        return not state[3]
-
-def try_new(new):
-    old_region_type = get_region_type(current)
-    new_region_type = get_region_type(new)
-    if not is_valid(old_region_type, new):
-        return
-    if not is_valid(new_region_type, new):
-        return
-    distance = 1
-    if current[2] != new[2] or current[3] != new[3]:
-        distance += 7 
-    new_distance = distances[current] + distance
-    if new not in distances or new_distance < distances[new]:
-        distances[new] = new_distance
-        active[new] = new_distance
-
-def try_news(new):
-    try_new((new[0], new[1], False, False))
-    try_new((new[0], new[1], False, True))
-    try_new((new[0], new[1], True, False))
-
-def get_next(distances, visited):
-    unvisited = [(k, v) for k, v in active.items()]
-    return min(unvisited, key=itemgetter(1))[0]
-
-while (tx, ty, True, False) not in visited:
+while current != target:
     if current[0] > 0:
-        try_news((current[0] - 1, current[1], current[2], current[3]))
+        try_news((current[0] - 1, current[1]))
     if current[1] > 0:
-        try_news((current[0], current[1] - 1, current[2], current[3]))
+        try_news((current[0], current[1] - 1))
     if current[0] < 80:
-        try_news((current[0] + 1, current[1], current[2], current[3]))
-    try_news((current[0], current[1] + 1, current[2], current[3]))
+        try_news((current[0] + 1, current[1]))
+    try_news((current[0], current[1] + 1))
 
-    visited.add(current)
-    if len(distances) % 1000 == 0:
-        print(len(distances), current[1])
-
-    current = get_next(distances, visited)
+    current = min(active.items(), key=itemgetter(1))[0]
     del active[current]
 
-print('Part 2: {}'.format(distances[(tx, ty, True, False)]))
+print('Part 2: {}'.format(distances[target]))
